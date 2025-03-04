aa0990848d15c0851a20972bd2b596d78f997aed
Kumari, Ankita (ak7846) <ak7846@att.com>
2025-03-04 10:40:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(()=>{
+      const timerId = setInterval(()=>setTime(new Date()),1000)
+      return (()=> clearInterval(timerId))
+  },[]);
+  
+  const formattedTime = time.toLocaleTimeString('en-US',
+    {
+      hour : '2-digit',
+      minute : '2-digit',
+      second : '2-digit'
+    }
+  )
+  return (
+    <>
+      <h1>Welcome</h1>
+      {formattedTime}
+    </>
+  )
+}
+
+export default App
